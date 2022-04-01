6b42cd46fa7cb8d4081c6fbf5d471ca50feee182
Forofonof <DanielResult1@outlook.com>
2022-04-01 20:10:00 +0300

    Добавьте файлы проекта.

---
+++ Homework_06/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+    internal class Program
+{
+    static void Main(string[] args)
+    {
+        int minutesInHour = 60;
+        int timeOneHuman = 10;
+        int allPeople;
+        int allTimeHour;
+        int allTimeMinute;
+
+        Console.WriteLine($"Сколько человек стоит перед вами? Среднее время одного человека в очереди: {timeOneHuman} минут.");
+        Console.WriteLine("Введите количество человек:");
+        allPeople = Convert.ToInt32(Console.ReadLine());
+
+        allTimeHour = allPeople * timeOneHuman / minutesInHour;
+        allTimeMinute = allPeople * timeOneHuman % minutesInHour;
+
+        Console.WriteLine($"Вам осталось ждать: {allTimeHour} час(а). {allTimeMinute} минут(ы).");
+    }
+}

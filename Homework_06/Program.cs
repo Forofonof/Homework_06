fa3f3d2648a26ea985df56612eaa0a893c459b21
Forofonof <DanielResult1@outlook.com>
2022-04-01 20:17:16 +0300

    Fix hourOfWaiting minutesOfWaiting

--- Homework_06/Program.cs
+++ Homework_06/Program.cs
@@ -7,16 +7,16 @@
         int minutesInHour = 60;
         int timeOneHuman = 10;
         int allPeople;
-        int allTimeHour;
-        int allTimeMinute;
+        int hourOfWaiting;
+        int minutesOfWaiting;
 
         Console.WriteLine($"Сколько человек стоит перед вами? Среднее время одного человека в очереди: {timeOneHuman} минут.");
         Console.WriteLine("Введите количество человек:");
         allPeople = Convert.ToInt32(Console.ReadLine());
 
-        allTimeHour = allPeople * timeOneHuman / minutesInHour;
-        allTimeMinute = allPeople * timeOneHuman % minutesInHour;
+        hourOfWaiting = allPeople * timeOneHuman / minutesInHour;
+        minutesOfWaiting = allPeople * timeOneHuman % minutesInHour;
 
-        Console.WriteLine($"Вам осталось ждать: {allTimeHour} час(а). {allTimeMinute} минут(ы).");
+        Console.WriteLine($"Вам осталось ждать: {hourOfWaiting} час(а). {minutesOfWaiting} минут(ы).");
     }
 }
